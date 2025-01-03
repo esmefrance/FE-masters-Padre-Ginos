c08c7425a6b8a24a4cff1f5c81b6744bd6f61722
esmefrance <esmereldaroses@gmail.com>
2025-01-03 17:58:22 +0000

    Create Order Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
+import Order from "./Order";
 
 const App = () => {
   return (
     <div>
       <h1>Padre Gino's - Order Now</h1>
-      <Pizza name="Margharita" description="Mozzarella cheese, basil, Tomato Base"/>
-      <Pizza name="Veggie Hawaiian" description="Mozzarella Cheese, Pineapple, Veggie Ham, Tomato Base"/>
-      <Pizza name="Veg Feast" description="Mozzarella Cheese, Red Onion, Sweetcorn, Peppers, Courgette, Pesto Drizzle, Tomato Base"/>
-      <Pizza name="Fiorentina" description="Mozzarella Cheese, Parmesan, Spinach, Black Olives, Egg, Tomato Base"/>
+      <Order />
     </div>
-  )
-}
+  );
+};
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App/>);
+root.render(<App />);
